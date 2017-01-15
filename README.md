4c3c216a38bdcd5ceafd5db3bed526c9e37cc0a8
hbdesiato <hbdesiato@gmx.de>
2017-01-15 02:02:41 +0100

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # docker-tomcat-ssl
+
+### Usage
+```bash
+docker run -p 8080:8080 -p 8443:8443 \
+  -v tomcat_conf:/usr/local/tomcat/conf \
+  -e HOST=tomcat.example.com \
+  desiato/tomcat-ssl
+```
---
+++ start.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+if [ ! -e "$CATALINA_HOME/conf/ssl/keystore.jks" ]; then
+	mkdir -p "$CATALINA_HOME/conf/ssl"
+	chmod go-rwx "$CATALINA_HOME/conf/ssl"
+	keytool -keystore "$CATALINA_HOME/conf/ssl/keystore.jks" \
+	-alias mykey -genkey -keyalg RSA -sigalg SHA256withRSA \
+	-validity 1095 -storepass changeit -keypass changeit \
+	-ext BC:true=ca:true \
+	-dname "cn=$HOST"
+fi
+catalina.sh run
