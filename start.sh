a07d52954e156d37b28e094a2db4594591026871
hbdesiato <hbdesiato@gmx.de>
2017-01-16 22:13:19 +0100

    import self-signed certificate as trust anchor

--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ if [ ! -e "$CATALINA_HOME/conf/ssl/keystore.jks" ]; then
 	-validity 1095 -storepass changeit -keypass changeit \
 	-ext BC:true=ca:true \
 	-dname "cn=$HOST"
+	
+	keytool -exportcert -keystore "$CATALINA_HOME/conf/ssl/keystore.jks" -storepass changeit -file "$CATALINA_HOME/conf/ssl/selfsigned.cer"
+	keytool -importcert -keystore "$CATALINA_HOME/conf/ssl/keystore.jks" -storepass changeit -file "$CATALINA_HOME/conf/ssl/selfsigned.cer" -alias ca -noprompt
 fi
 exec catalina.sh run
